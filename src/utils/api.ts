415d522db38a045bd1bd45e4d48d7277154a05d3
Jason Wang <41667764+jawang94@users.noreply.github.com>
2021-01-07 04:02:31 -0800

    hardcoding api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 const baseUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080/api'
-    : `${process.env.DATABASE_ENDPOINT}/api`;
+    : 'https://jawang94-personal-site-server.herokuapp.com/api';
 
 const methods = {
   async get(endpoint: string, token?: any) {
